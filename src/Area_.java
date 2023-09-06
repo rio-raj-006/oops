4fa7544082056595277b70b0c2c1bee9c4e1c751
projectsanjay <sanjay261101@gmail.com>
2023-09-06 20:47:31 +0530

    commit by sanjay

--- src/Area_.java
+++ src/Area_.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 class Maain{
 public static void main(String[] args){
-
-        Scanner s2 = new Scanner(System.in);
+// edited by sanjay
+        Scanner s = new Scanner(System.in);
         System.out.println("Enter the length :");
-        int l=s2.nextInt();
+        int l=s.nextInt();
         System.out.println("Enter the breadth");
-        int b=s2.nextInt();
+        int b=s.nextInt();
         Area_ obj=new Area_();
         obj.setDim(l,b);
     System.out.println(obj.getArea());
