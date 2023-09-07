fcfe4a34e21d96ba613c3837b7f2b0d0249dc7d2
Raj <rajkumar.selvaraj@zucisystems.com>
2023-09-07 10:26:48 +0530

    new one

---
+++ src/forloop.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class forloop {
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        for(int i=0;i<a;i++){
+            System.out.println("*");
+        }
+    }
+}
