a0954d2765232eb33a098fe252beca0a85cf18b2
Raj <rajkumar.selvaraj@zucisystems.com>
2023-09-07 10:35:28 +0530

    new

--- src/forloop.java
+++ src/forloop.java
@@ -4,8 +4,12 @@ public class forloop {
     public static void main(String[] args){
         Scanner s=new Scanner(System.in);
         int a=s.nextInt();
-        for(int i=0;i<a;i++){
-            System.out.println("*");
+        for(int i=0;i<a;i++){            // for-loop statement, we use for loop when we know about the limit of the loop
+            System.out.print("*");
+        }
+        while(a>0){          // while-loop, we use while loop when we know the limit of loop
+            a=a-2;
+            System.out.println("#");
         }
     }
 }
